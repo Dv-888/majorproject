660e8b8636ddb1cf4b44b1ef2c6c78c9666f7e04
Dv <dhruv007rawat@gmail.com>
2021-05-24 22:50:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:farmerapp/homepage.dart';
+import 'package:farmerapp/login.dart';
+import 'package:farmerapp/start.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'SignUp.dart';
+import 'onboarding.dart';
+import 'start.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: HomePage(),
+
+      routes: <String,WidgetBuilder>{
+
+        "Login" : (BuildContext context)=>Login(),
+        "SignUp":(BuildContext context)=>SignUp(),
+        "start":(BuildContext context)=>Start(),
+      },
+      
+    );
+  }
+}
---
+++ lib/main_drawer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class MainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            color: Colors.black38,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(margin: EdgeInsets.only(top: 20),
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                image: AssetImage("assets/images/bg_01_2.png"),
+                fit: BoxFit.cover),
+                  )),
+                  
+                  Text('hello'),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
